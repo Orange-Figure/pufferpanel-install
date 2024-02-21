0d9a10e6c1525bc6c3a2f892d85ced8f911d78b3
Orange Figure <137558630+Orange-Figure@users.noreply.github.com>
2024-02-21 15:55:03 +0400

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 # FOR x86 servers using linux : 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 2XWvdIQegnz7fRUXkMvJWVy7Z5p_3SijirFvadrRgBJot32fG #Put Yours here
+./ngrok config add-authtoken 2ce0o9oQ2jj5Th9Yz9A0tfQzE9e_5nVeKp8aX64HkidxXDsww #Put Yours here
 ./ngrok http 8080
